baf0d0406c2f533fbdecb78fcc0748a03f1c100d
Aaron Sanchez <saarong12@gmail.com>
2026-01-31 01:55:14 -0600

    Modified demo code to get the stats for a single running container.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"log"
 
+	"github.com/moby/moby/api/types/container"
 	"github.com/moby/moby/client"
 )
 
@@ -17,15 +20,41 @@ func main() {
 	}
 	defer apiClient.Close()
 
-	images, err := apiClient.ImageList(ctx, client.ImageListOptions{})
+	testContainerID := "49283dece168"
+
+	stats, err := apiClient.ContainerStats(ctx, testContainerID, client.ContainerStatsOptions{Stream: true})
 	if err != nil {
 		panic(err)
 	}
+	defer stats.Body.Close()
 
-	fmt.Println("Images: ")
+	decoder := json.NewDecoder(stats.Body)
 
-	for _, image := range images.Items {
-		fmt.Println(image.RepoTags)
+	for {
+		var s container.StatsResponse
+
+		if err := decoder.Decode(&s); err != nil {
+			log.Fatal(err)
+		}
+
+		// MEM%
+		usedMem := s.MemoryStats.Usage - s.MemoryStats.Stats["inactive_file"]
+		memPercent := (float64(usedMem) / float64(s.MemoryStats.Limit)) * 100
+		sprint := fmt.Sprintf("%.2f", memPercent)
+
+		usedMemInMb := usedMem / 1048576
+
+		// TODO: Need to modify the cpu percent to display in decimal point.
+		// CPU Usage
+		cpuD := s.CPUStats.CPUUsage.TotalUsage
+		systemCpuD := s.CPUStats.SystemUsage - s.PreCPUStats.SystemUsage
+		numCpu := s.CPUStats.OnlineCPUs
+		cpuPercent := (cpuD / systemCpuD) * uint64(numCpu) * 100
+
+		fmt.Printf("Memory Percent : %v%%, Mem Usage: %dmb, cpu usage: %d\n",
+			sprint,
+			usedMemInMb,
+			cpuPercent)
 	}
 
 }
