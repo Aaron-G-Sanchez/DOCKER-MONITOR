e4480eaf48b61a94f55575494b330995052847e3
Aaron Sanchez <saarong12@gmail.com>
2026-02-01 23:25:15 -0600

    Added date and time for time specific logging.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/moby/moby/api/types/container"
 	"github.com/moby/moby/client"
@@ -37,6 +38,42 @@ func main() {
 			log.Fatal(err)
 		}
 
+		// Time
+		cur := time.Now()
+		hr, min, sec := cur.Clock()
+		y, m, d := cur.Date()
+		var monthNum int
+		switch m {
+		case time.January:
+			monthNum = 1
+		case time.February:
+			monthNum = 2
+		case time.March:
+			monthNum = 3
+		case time.April:
+			monthNum = 4
+		case time.May:
+			monthNum = 5
+		case time.June:
+			monthNum = 6
+		case time.July:
+			monthNum = 7
+		case time.August:
+			monthNum = 8
+		case time.September:
+			monthNum = 9
+		case time.October:
+			monthNum = 10
+		case time.November:
+			monthNum = 11
+		case time.December:
+			monthNum = 12
+		default:
+			panic("Invalid month")
+		}
+
+		dateTime := fmt.Sprintf("%v-%v-%v %v:%v:%v", y, monthNum, d, hr, min, sec)
+
 		// MEM%
 		usedMem := s.MemoryStats.Usage - s.MemoryStats.Stats["inactive_file"]
 		memPercent := (float64(usedMem) / float64(s.MemoryStats.Limit)) * 100
@@ -51,7 +88,8 @@ func main() {
 		numCpu := s.CPUStats.OnlineCPUs
 		cpuPercent := (cpuD / systemCpuD) * uint64(numCpu) * 100
 
-		fmt.Printf("Memory Percent : %v%%, Mem Usage: %dmb, cpu usage: %d\n",
+		fmt.Printf("Time: %v, Memory Percent : %v%%, Mem Usage: %dmb, cpu usage: %d\n",
+			dateTime,
 			sprint,
 			usedMemInMb,
 			cpuPercent)
